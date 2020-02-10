042322169c39962ec86c66c2fc87f7bea1c91faf
Grant Steinfeld <Grant.Steinfeld.Tech@gmail.com>
2020-02-09 23:21:57 -0500

    main is now flask api entrypoint

--- src/currencyexchange/src/main.py
+++ src/currencyexchange/src/main.py
@@ -1,15 +1,27 @@
+from flask import Flask
+from services.serviceHandler import convertCurrency
 import logging
 
+logger = logging.getLogger(__name__)
+app = Flask(__name__)
 
-def main():
-    """  testing logger """
-    logger.info("starting")
+
+# /currency/{currencyFromAmount}/{currencyFromCode}/{currencyToCode}
+#  /currency/10/EUR/USD
+
+
+@app.route("/currency/<currencyFromAmount>/<currencyFromCode>/<currencyToCode>")
+def getCurrency(currencyFromAmount, currencyFromCode, currencyToCode):
+    result = convertCurrency(
+        float(currencyFromAmount), currencyFromCode, currencyToCode
+    )
+    return {"result": result}
 
 
 if __name__ == "__main__":
     """ this should be in the program's main/start/run function """
-    import logging.config
+    # import logging.config
 
-    logging.config.fileConfig("logging.conf")
-    logger = logging.getLogger(__name__)
-    main()
+    # logging.config.fileConfig("logging.conf")
+    # logger = logging.getLogger(__name__)
+    app.run(debug=True)
