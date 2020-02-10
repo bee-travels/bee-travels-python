95ae532859305ba1182d91a0615884b004466306
Grant Steinfeld <Grant.Steinfeld.Tech@gmail.com>
2020-02-10 00:38:03 -0500

    flask-restplus now works as expected

--- src/currencyexchange/src/main.py
+++ src/currencyexchange/src/main.py
@@ -1,27 +1,34 @@
 from flask import Flask
+from flask_restplus import Api, Resource
 from services.serviceHandler import convertCurrency
-import logging
 
-logger = logging.getLogger(__name__)
 app = Flask(__name__)
+api = Api(
+    app,
+    version="1.0.0",
+    title="Bee Travel Currency Data Service",
+    description="This is a microservice that handles currency exchange rate data for Bee Travels",
+)
 
+currencyNS = api.namespace("currency", description="currency exchange operations")
 
-# /currency/{currencyFromAmount}/{currencyFromCode}/{currencyToCode}
+#  /currency/{currencyFromAmount}/{currencyFromCode}/{currencyToCode}
 #  /currency/10/EUR/USD
 
 
-@app.route("/currency/<currencyFromAmount>/<currencyFromCode>/<currencyToCode>")
-def getCurrency(currencyFromAmount, currencyFromCode, currencyToCode):
-    result = convertCurrency(
-        float(currencyFromAmount), currencyFromCode, currencyToCode
-    )
-    return {"result": result}
+@currencyNS.route("/<int:currencyFromAmount>/<currencyFromCode>/<currencyToCode>")
+@currencyNS.response(404, "Currency Code not found")
+@currencyNS.param("currencyFromAmount", "currency to convert from value (float)")
+@currencyNS.param("currencyFromCode", "currency (3 character code) to convert from")
+@currencyNS.param("currencyToCode", "currency (3 character code) to convert to")
+class Currency(Resource):
+    def get(self, currencyFromAmount, currencyFromCode, currencyToCode):
+
+        result = convertCurrency(
+            float(currencyFromAmount), currencyFromCode, currencyToCode
+        )
+        return {"result": result}
 
 
 if __name__ == "__main__":
-    """ this should be in the program's main/start/run function """
-    # import logging.config
-
-    # logging.config.fileConfig("logging.conf")
-    # logger = logging.getLogger(__name__)
-    app.run(debug=True)
+    app.run(host="127.0.0.1", debug=True, port=7878)
